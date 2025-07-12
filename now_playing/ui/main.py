8f5d6007426d1706c9d0ddaa4a7092975815231a
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2025-07-13 00:08:33 +1000

    (ui) prototype `MediaList`

--- now_playing/ui/main.py
+++ now_playing/ui/main.py
@@ -1,35 +1,56 @@
 from typing import List
 
 from PySide6.QtCore import Qt
+from PySide6.QtSvgWidgets import QSvgWidget
 from PySide6.QtWidgets import (
     QApplication,
     QGridLayout,
     QLabel,
-    QWidget,
-    )
+    QScrollArea,
+    QWidget)
 
 
 # TODO: grid layout calendar
 # -- populate each day w/ info from db
 # -- episode release dates etc.
 
+# TODO: make it a button
+class MediaEntry(QWidget):
+    def __init__(self, title: str, parent=None):
+        super().__init__(parent)
 
+        icon = QSvgWidget("./svg/vinyl.33.svg")
+        # icon = QSvgWidget("./svg/vinyl.45.svg")
+        # icon = QSvgWidget("./svg/vinyl.78.svg")
+        label = QLabel(title)
+        label.setAlignment(Qt.AlignCenter)
+        label.setStyleSheet("""
+            padding: 1.5rem;
+            background-color: #333;
+            color: #CCC;
+            font-family: sans-serif;
+            font-size: 24px;""")
+
+        layout = QGridLayout()
+        layout.addWidget(icon, 0, 0)
+        layout.addWidget(label, 1, 0)
+        self.setLayout(layout)
+
+
+# TODO: resize to fit widget
+# -- might involve adjusting layout row & column counts
 class MediaList(QWidget):
+    # TODO: manage layout rows & columns
+    # -- handles to push & pop entries
+    # -- scrollable (QScrollArea?)
+    # -- maintain proportions of entries
+
     def __init__(self, parent=None):
-        super(MediaList, self).__init__(parent)
+        super().__init__(parent)
 
         layout = QGridLayout()
         for i in range(8):
-            # TODO: use a icon box / podcast icon widget
-            # -- pull up some menu when clicked
-            label = QLabel(f"Episode {i:03d}")
-            label.setAlignment(Qt.AlignCenter)
-            label.setStyleSheet("""
-                padding: 1.5rem;
-                background-color: #333;
-                color: #CCC;
-                font-family: sans-serif;
-                font-size: 24px;""")
+            label = MediaEntry(f"Episode {i:03d}")
             x = i % 3
             y = i // 3
             layout.addWidget(label, y, x)
@@ -41,7 +62,15 @@ def main(argv: List[str]):
     """sys.exit(main(sys.argv))"""
     app = QApplication(argv)
 
-    test_widget = MediaList()
-    test_widget.show()
+    # TODO: contain scrollarea in a parent widget
+    # -- add a filter section
+
+    # scrollable list of media
+    media_list = MediaList()
+    main_widget = QScrollArea()
+    main_widget.setWidget(media_list)
+    main_widget.show()
+
+    # TODO: visual queue organiser
 
     return app.exec()
